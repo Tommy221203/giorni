aeaf6561c8467a9ed6dd0697a2d44962528ab576
Tommy221203 <55913204+Tommy221203@users.noreply.github.com>
2019-10-27 18:58:06 +0100

    finito

--- giorni/Program.cs
+++ giorni/Program.cs
@@ -6,7 +6,33 @@ namespace giorni
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("quale giorno vuoi selezionare? 1/2/3/4/5/6/7");
+            string giorno = Console.ReadLine();
+            switch(giorno)
+            {
+                case "1":
+                    Console.WriteLine("domenica");
+                    break;
+                case "2":
+                    Console.WriteLine("lunedi");
+                    break;
+                case "3":
+                    Console.WriteLine("martedi");
+                    break;
+                case "4":
+                    Console.WriteLine("mercoledi");
+                    break;
+                case "5":
+                    Console.WriteLine("giovedi");
+                    break;
+                case "6":
+                    Console.WriteLine("venerdi");
+                    break;
+                case "7":
+                    Console.WriteLine("sabato");
+                    break;
+            }
+            Console.ReadLine();
         }
     }
 }
